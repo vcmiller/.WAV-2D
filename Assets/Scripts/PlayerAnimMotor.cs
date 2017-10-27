2d5bac1fd253b0dcd83fa1208d484534a2c75cc4
Vincent Miller <vmiller0725@gmail.com>
2017-10-27 14:05:43 -0400

    Player animations

---
+++ Assets/Scripts/PlayerAnimMotor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimMotor : BasicMotor<CharacterProxy> {
+    public Animator anim { get; private set; }
+    public SpriteRenderer sprite { get; private set; }
+    public CharacterMotor2D motor { get; private set; }
+
+    private State attackState;
+    private ExpirationTimer attack;
+
+    public override void TakeInput() {
+        if (control.movement.x != 0 && attack.expired) {
+            sprite.flipX = control.movement.x < 0;
+        }
+
+        if (!attack.expired) {
+            Play(attackState);
+        } else if (!motor.grounded) {
+            Play(State.Jump);
+        } else if (control.movement.x == 0) {
+            Play(State.Idle);
+        } else {
+            Play(State.Run);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            attack.Set();
+
+            if (control.movement.y > 0) {
+                attackState = State.SawUp;
+            } else if (control.movement.y < 0 && !motor.grounded) {
+                attackState = State.SawDown;
+            } else {
+                attackState = State.SawSide;
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        motor = GetComponent<CharacterMotor2D>();
+
+        attack = new ExpirationTimer(0.3f);
+    }
+
+    public enum State {
+        Idle, Run, SawSide, SawUp, SawDown, Jump
+    }
+
+    private void Play(State state) {
+        anim.Play(state.ToString());
+    }
+}
--- Assets/SlightlyBetterRats/Control/Character/SampleCharacterController2D.cs
+++ Assets/SlightlyBetterRats/Control/Character/SampleCharacterController2D.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class SampleCharacterController2D : PlayerController<CharacterProxy> {
 	
 	public void Axis_Horizontal(float value) {
-        Vector3 right = transform.right;
-        right.y = 0;
-        right = right.normalized;
 
-        controlled.movement += right * value;
+        controlled.movement += Vector3.right * value;
+    }
+
+    public void Axis_Vertical(float value) {
+        controlled.movement += Vector3.up * value;
     }
 
     public void ButtonDown_Jump() {
