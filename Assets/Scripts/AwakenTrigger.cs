1ff943016776568bd7e32911ef17d7b72ad38976
Vincent Miller <vmiller0725@gmail.com>
2017-10-29 22:31:39 -0400

    refactored some stuff

---
+++ Assets/Scripts/AwakenTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakenTrigger : MonoBehaviour {
+    public GameObject[] targets;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            foreach (var obj in targets) {
+                obj.SendMessage("Awaken", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
