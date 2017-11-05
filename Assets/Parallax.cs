e251140a5366a5757c1d8107e4bb3df060152a98
Vincent Miller <vmiller0725@gmail.com>
2017-11-05 18:08:21 -0500

    2d parallax stuff

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[ExecuteInEditMode]
 public class Parallax : MonoBehaviour {
-    public Camera camera;
 
     [Range(0, 1)]
     public float parallaxFactor;
-
-    private void LateUpdate() {
-        transform.position = camera.transform.position * parallaxFactor;
+    
+    private void OnRenderObject() {
+        Vector3 v = Camera.current.transform.position * parallaxFactor;
+        v.z = 0;
+        transform.position = v;
     }
 }
---
+++ Assets/ParallaxParticles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxParticles : MonoBehaviour {
+    public Transform camera;
+    public Vector3 offset;
+
+    private ParticleSystem.ShapeModule shape;
+
+	// Use this for initialization
+	void Start () {
+        shape = GetComponent<ParticleSystem>().shape;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shape.position = camera.position + offset - transform.position;
+	}
+}
