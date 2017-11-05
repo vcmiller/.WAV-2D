f4ba9f7b2366bef101f1b9e401cee32e63ee3f60
Vincent Miller <vmiller0725@gmail.com>
2017-11-04 23:48:26 -0400

    mostly art stuff

---
+++ Assets/CopyCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyCamera : MonoBehaviour {
+    public Camera otherCamera;
+
+    private Camera myCamera;
+
+    private void Awake() {
+        myCamera = GetComponent<Camera>();
+    }
+
+    private void LateUpdate() {
+        myCamera.transform.position = otherCamera.transform.position;
+        myCamera.orthographicSize = otherCamera.orthographicSize;
+    }
+}
---
+++ Assets/Parallax.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    public Camera camera;
+
+    [Range(0, 1)]
+    public float parallaxFactor;
+
+    private void LateUpdate() {
+        transform.position = camera.transform.position * parallaxFactor;
+    }
+}
